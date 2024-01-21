ce49576e3c0f0d6c576e121417d296ecdf475332
HugoHenrique1 <hugohenriquedev@gmail.com>
2024-01-21 13:11:01 -0300

    Jogo de adivinhar um númro

---
+++ app.js
@@ -0,0 +1,23 @@
+alert('Boas vindas ao jogo do número secreto');
+let numeroMaximo = 30
+let numeroSecreto = parseInt(Math.random() * numeroMaximo + 1);
+console.log(numeroSecreto)
+let chute;
+let tentativas = 1;
+
+while (chute != numeroSecreto) {
+    chute = prompt(`Escolha um número entre 1 a ${numeroMaximo}`);
+
+    if (chute == numeroSecreto) {
+        break;
+    } else {
+        if (chute > numeroSecreto) {
+            alert(`O número secreto é menor que ${chute}`);
+        } else {
+            alert(`o número secreto é maior que ${chute}`);
+        }
+        tentativas++;
+    }
+}
+let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
+alert(`Isso ai! você descobriu o número secreto ${numeroSecreto} com ${tentativas} ${palavraTentativa}.`);
